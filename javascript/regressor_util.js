1e7b73cf90e08ae32e01cb4ac832bfc45c6fc51a
Raj Gaurav <singhggaurav14@gmail.com>
2024-05-17 12:38:11 +0530

    organized and added buttons for GD

---
+++ javascript/regressor_util.js
@@ -0,0 +1,65 @@
+export class Regressor {
+    constructor(mode, c) {
+        this.c = c
+        this.m = (Math.random() * 2 - 1) * 2
+        this.b = (Math.random() * 2 - 1) * 100
+        this.starting_m = this.m
+        this.starting_b = this.b
+        this.step = 0
+        this.mode = mode
+    }
+    regress(data, c) {
+        if (this.mode == 'OLS') {
+            var sum = 0
+            var sum2 = 0
+            for (let i = 0; i < data.X.length; i++) {
+                sum += data.X[i]
+                sum2 += data.Y[i]
+            }
+            var x_mean = sum / data.X.length
+            var y_mean = sum2 / data.Y.length
+
+            var num = 0
+            var den = 0
+            for (let i = 0; i < data.X.length; i++) {
+                num += (data.X[i] - x_mean) * (data.Y[i] - y_mean)
+                den += (data.X[i] - x_mean) * (data.X[i] - x_mean)
+            }
+            this.m = num / den
+            this.b = y_mean - this.m * x_mean
+        }
+        else if (this.mode == 'SGD'){
+            // to regress
+        }
+
+    }
+    loss(data){
+        var sumer = 0
+        for (let i=0; i<data.X.length;i++){
+            sumer += data.Y[i]-(this.m*data.X[i]+this.b)*(this.m*data.X[i]+this.b)
+        }
+        return sumer/data.X.length
+    }
+
+    show_errors(data) {
+        for (let i = 0; i < data.X.length; i++) {
+            var x1 = data.X[i]
+            var y1 = data.Y[i]
+            var x2 = data.X[i]
+            var y2 = this.m * data.X[i] + this.b
+            this.c.line(x1 + this.c.width * .5, y1 + this.c.height * .5, x2 + this.c.width * .5, y2 + this.c.height * .5, 1, 'red')
+        }
+    }
+    draw_line() {
+        var x = this.c.width * .5
+        var y = this.c.height * .5
+        var x1 = -x
+        var y1 = (this.m * (-x) + this.b)
+        var x2 = x
+        var y2 = (this.m * (x) + this.b)
+        // console.log(x1,y1,x2,y2)
+        this.c.line(x1 + x, y1 + y, x2 + x, y2 + y, 3, 'green')
+    }
+
+}
+
--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 import { Canvas } from './canvas_util.js';
-import { Data } from './regressor_util.js';
+import { Data } from './data.js';
 import { Regressor } from './regressor_util.js';
 
 var c = new Canvas(0.754, 0.886)
@@ -8,35 +8,74 @@ function getFromId(id){
     return document.getElementById(id);
 }
 
-// select
-var regresion_mode = getFromId('regresion_mode')
-regresion_mode.addEventListener('input',()=>{
-    // to-do change
-})
-
 // slider
 var numberofdatapointsEmt = getFromId("numberOfDataPoints")
+var total_epoches_num = getFromId("total_epoches")
+var learning_Rate = getFromId("learning_Rate")
 var val_m = getFromId("val_m")
 var val_b = getFromId("val_b")
 
 // viwew
 var datapoints_number = getFromId("numberOfDataPointsView")
+var total_epoches_view = getFromId("total_epoches_show")
+var current_epoch = getFromId("current_epoch")
+var learning_Rate_view = getFromId("learning_rate_show")
 var val_m_show = getFromId("val_m_show")
 var val_b_show = getFromId("val_b_show")
+// seting Default view
+total_epoches_view.textContent = 'Total Epoches : 10'
+learning_Rate_view.textContent = 'learning Rate : 0.1'
+val_m_show.textContent = 'value of m : 0'
+val_b_show.textContent = 'value of b : 0'
+current_epoch.textContent = 'Current Epoch : 0'
 
 // buttons
 var new_data_button = getFromId("newData")
 var regressorEmt = getFromId("regress")
+var regress_gradient = getFromId("regress_gradient")
 var change_data=false
 var regress=false
+var regress_gradi=false
 new_data_button.addEventListener('click',()=>{change_data=true})
 regressorEmt.addEventListener('click',()=>{regress=true})
+regress_gradient.addEventListener('click',()=>{regress_gradi=true})
 
 // checkboxes
 var showGridsEmt = getFromId("show_grid")
 var showError = getFromId("distance")
 showGridsEmt.checked=true
 showError.checked=false
+
+// select
+var regresion_mode = getFromId('regresion_mode')
+regresion_mode.addEventListener('input',()=>{
+    // to-do change
+    if (regresion_mode.value==="OLS"){
+        // in OLS MODE
+        console.log("ols_MODE")
+        // showing
+        regressorEmt.style.display='block';
+        val_m.style.display='block';
+        val_m_show.style.display='block';
+        val_b.style.display='block';
+        val_b_show.style.display='block';
+    }
+    else if (regresion_mode.value==="SGD"){
+        // in SGD MODE
+        console.log("SGD_MODE")
+        // hiding
+        regressorEmt.style.display='none';
+        val_m.style.display='none';
+        val_m_show.style.display='none';
+        val_b.style.display='none';
+        val_b_show.style.display='none';
+
+    }
+    else{
+        // in Polynomial MODE
+        console.log("polynomial mode")
+    }
+})
 
 document.addEventListener('contextmenu', event => event.preventDefault());
 
@@ -58,7 +97,7 @@ function create_Grid(){
 }
 
 var data = new Data(numberofdatapointsEmt.value,c)
-var regressor = new Regressor(c)
+var regressor = new Regressor('OLS', c)
 
 numberofdatapointsEmt.addEventListener("input", ()=>{
     datapoints_number.textContent = numberofdatapointsEmt.value
@@ -66,16 +105,19 @@ numberofdatapointsEmt.addEventListener("input", ()=>{
     data.trim(new_num)
 })
 val_m.addEventListener('input', ()=>{
-    console.log(val_m.value, typeof(val_m.value))
-    val_m_show.textContent = +val_m.value
+    val_m_show.textContent = 'value of m : ' + val_m.value
     regressor.m = +val_m.value
 })
 val_b.addEventListener('input', ()=>{
-    val_b_show.textContent = +val_b.value
+    val_b_show.textContent = 'value of b : ' + val_b.value
     regressor.b = +val_b.value
 })
-
-
+total_epoches_num.addEventListener('input', ()=>{
+    total_epoches_view.textContent ='Total Epoches : ' + total_epoches_num.value
+})
+learning_Rate.addEventListener('input',()=>{
+    learning_Rate_view.textContent = 'learning Rate : ' + learning_Rate.value
+})
 
 var button_ctrl=true
 function animate(){
@@ -94,16 +136,16 @@ function animate(){
         regress=false
         regressor.regress(data)
         val_m.value = regressor.m.toFixed(2)
-        val_m_show.textContent = regressor.m.toFixed(2)
+        val_m_show.textContent = 'value of m : ' + regressor.m.toFixed(2)
         val_b.value = regressor.b.toFixed(2)
-        val_b_show.textContent = regressor.b.toFixed(2)
+        val_b_show.textContent = 'value of b : ' + regressor.b.toFixed(2)
     }
     if (showError.checked){
         regressor.show_errors(data)
     }  
     data.draw()
     regressor.draw_line()
-
+    // console.log(regressor.loss(data))
     if (c.mouse_btn==0 && button_ctrl){
         if (c.mouse[0]>0 && c.mouse[1]>0){
             button_ctrl = false
